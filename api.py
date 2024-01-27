160155d297e0af470dc9ca6d0b4b9125e812fd5e
Chanmonyneath Has <chanmoh@uci.edu>
2024-01-26 22:18:46 -0800

    get_coordinate function returns latitude and longitude of named location
    Created get_coordinate function which takes a string location which calls nominatim API before turning lat and lon into a Coordinate namedtuple.

--- api.py
+++ api.py
@@ -1,10 +1,17 @@
 import urllib.request
 import urllib.parse
 import json
+from collections import namedtuple
 
-URL_COURSES = 'https://api.peterportal.org/rest/v0/courses/'
+FORWARD_SEARCH_URL = 'https://nominatim.openstreetmap.org/search?format=json&q='
+Coordinate = namedtuple('Coordinate', ['latitude', 'longitude'])
 
-def get_33():
-    request = urllib.request.Request(f'{URL_COURSES}I&CSCI33')
-    response = json.loads(urllib.request.urlopen(request).read().decode(encoding='utf-8'))
-    return response
+def get_coordinate(named_location: str) -> Coordinate:
+    named_location += ' University of California Irvine'
+    encoded_location = urllib.parse.quote(named_location)
+    request = urllib.request.Request(FORWARD_SEARCH_URL + encoded_location)
+    response = urllib.request.urlopen(request)
+    data = json.load(response)[0]
+    return Coordinate(data['lat'], data['lon'])
+
+
--- location.py
+++ location.py
@@ -1,6 +1,7 @@
 import geocoder
 
+
 user_location = geocoder.ip('me')
 
 def calculate_distance(location1: tuple(int, int), location2: tuple(int, int)) -> int:
-    
+    pass
