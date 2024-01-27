b1f0d9474d75cab8db810d117bba340e15f76623
raynmy <118318401+raynmya@users.noreply.github.com>
2024-01-26 22:28:53 -0800

    Add files via upload
    calculating distance

--- location.py
+++ location.py
@@ -1,7 +1,14 @@
 import geocoder
-
+from collections import namedtuple
+from math import cos, sin, degrees, arcsin, sqrt, pow
+Coordinate = namedtuple('Coordinate', ['latitude', 'longitude'])
 
 user_location = geocoder.ip('me')
 
-def calculate_distance(location1: tuple(int, int), location2: tuple(int, int)) -> int:
-    pass
+#d = 2R × sin⁻¹(√[sin²((θ₂ - θ₁)/2) + cosθ₁ × cosθ₂ × sin²((φ₂ - φ₁)/2)]).
+
+def calculate_distance(location1: Coordinate(int, int), location2: Coordinate(int, int)) -> int:
+
+    body = pow((sin(location2.latitude - location1.latitude)/2), 2) + cos(location2.latitude) * cos(location1.latitude) * pow(sin((location2.longitude - location1.longitude)/2, 2))
+    return int(2*6371 * arcsin(sqrt(body)))
+    
